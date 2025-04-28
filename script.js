3cd5de147a5d94fbbc5100b475d8a495620eaf6e
dmuslima <pirmukhamedovamuslima01@gmail.com>
2025-04-28 21:55:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let button = document.getElementById('theme-toggle');
+
+button.addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+});
+
+
+let button2 = document.getElementsById('btn');
+
+button.addEventListener('click', function() {
+    document.body.classList.toggle('light-mode');
+  });
